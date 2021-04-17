98902e36deabf107cee25d1d68e08d663cc201fa
Dalphs <Simon-dalby@hotmail.com>
2021-04-17 23:49:56 +0200

    early commit to keep streak, next commit will be the actual

--- src/Components/Podium/Podium.js
+++ src/Components/Podium/Podium.js
@@ -2,23 +2,43 @@ import {Container, Row, Col} from 'react-bootstrap';
 import {useRecoilState} from 'recoil';
 import './styles.css'
 import {castlesGameState, updateElement} from '../../recoil/castlesGameState.js'
-import {Button} from '@material-ui/core' 
+import {Button, ClickAwayListener} from '@material-ui/core' 
 import {useEffect} from 'react'
 
 export default function Podium (props) {
 
-    const [game, setGame] = useRecoilState(castlesGameState);
+    let currentScreen = [];
 
-    useEffect(() => {
-        console.log("kører Podium én gang")
-        let players = props.players;
-        console.log(players)
+    
+    let players = [...props.players];
+    let playersSorted= players.sort(function(x,y){return y["points"]-x["points"]});
+    
+    let actualNumberOfRuns = 1;
+    for (let i = 0; i < playersSorted.length; i++) {
+        let startIndex = i
+        let currentValue = playersSorted[i].points;
 
-    }, [])
+        for (let j = startIndex +1; j < playersSorted.length; j++) {
+            if(currentValue === playersSorted[j].points)
+                currentValue++
+            else    
+                break
+        }
+        switch (i) {
+            case 0:
+        }
+
+        i = startIndex;
+        actualNumberOfRuns++
+    }
+    currentScreen.push(<h1 key="uu">Virker</h1>)
+    currentScreen.push(<h2 key="pp">Det</h2>)
+    
+    
 
    
     
     return(
-        <Col><h1>Div</h1></Col>
+        <Col>{currentScreen}</Col>
     )
 }
